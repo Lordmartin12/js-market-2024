530050b6c82f0a764950feb6128282738d4dc515
destinybravos <destinybravos@gmail.com>
2025-02-04 17:47:30 +0100

    Introduction to React Hooks

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,7 +1,59 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 const Contact = () => {
+    const [showModal, setShowModal] = useState(false);
+    const [selectedUser, setSelectedUser] = useState(null);
+    const [users, setUsers] = useState([])
+
+    useEffect(() => {
+        axios.get('https://jsonplaceholder.typicode.com/users')
+            .then((res) => {
+                setUsers(res.data);
+            })
+
+        return () => {
+            setUsers([]);
+        }
+    }, [])
+
     return (
-        <div>
+        <div className="px-4 py-4 max-w-6xl mx-auto">
             <h1>Contact Page</h1>
+
+            <ul className="divide-y">
+                {users.length > 0 && users.map((user, index) => (
+                    <li key={index} role="button" onClick={() => { setSelectedUser(user); setShowModal(true)}} className="cursor-pointer py-2 px-4 shadow rounded-2xl">
+                        <div>
+                            <h3 className="font-semibold">{ user.name }</h3>
+                            <p className="text-xs italic">@{ user.username }</p>
+                            <small className="text-sm">{ user.email }</small>
+                        </div>
+                    </li>
+                ))}
+            </ul>
+
+            {/* Modal Section */}
+            { selectedUser && <div className={`fixed inset-0 z-10 bg-slate-950/80 ${showModal ? 'block' : 'hidden'}`}>
+                <div className="fixed inset-0 flex justify-center items-center">
+                    <div className="bg-white rounded-md p-4 w-full max-w-xl relative">
+                        <button role="button" className="absolute top-2 right-3 py-2 px-4 bg-red-500 text-white rounded-md" onClick={() => setShowModal(false)}>
+                            Close
+                        </button>
+                        
+                         <section className="space-y-2">
+                            <h3 className="font-semibold">{ selectedUser.name }</h3>
+                            <p className="text-xs italic">@{ selectedUser.username }</p>
+                            <p className="text-sm">{ selectedUser.email }</p>    
+                            <p className="text-sm">{ selectedUser.phone }</p>    
+                            <p className="text-sm">
+                                { selectedUser.address.suite }, { selectedUser.address.street }, { selectedUser.address.city }
+                            </p>    
+                        </section> 
+                    </div>
+                </div>
+            </div> }
+
         </div>
     )
 }
--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,7 +1,52 @@
+import { useState } from "react";
 const Index = () => {
+    const [number, setNumber] = useState(0);
+    const [num, setNum] = useState(0);
+    
+    const increment = () => {
+        setNumber(number + 1);
+    }
+
+    const decreement = () => {
+        if (number > 0) {
+            setNumber(number - 1);
+        }
+    }
+
     return (
-        <div>
-            <h1>Index Page</h1>
+        <div className="px-4 py-4 max-w-6xl mx-auto">
+
+            <section className="flex gap-4 items-center">
+                <button onClick={() => decreement()} className="py-1 px-2 bg-slate-300 text-2xl rounded-md flex justify-center items-center">
+                    -
+                </button>
+
+                <div>
+                    { number }
+                </div>
+
+                <button onClick={() => increment()} className="py-1 px-2 bg-slate-300 text-2xl rounded-md flex justify-center items-center">
+                    +
+                </button>
+            </section>
+
+            <section className="mt-8">
+                <span className="text-7xl"> { number } </span>
+            </section>
+
+            <section className="flex gap-4 items-center mt-4">
+                <button onClick={() => setNum((current) => (current > 0) ? current - 1 : 0)} className="py-1 px-2 bg-slate-300 text-2xl rounded-md flex justify-center items-center">
+                    -
+                </button>
+
+                <div>
+                    { num }
+                </div>
+
+                <button onClick={() => setNum((current) => current + 1)} className="py-1 px-2 bg-slate-300 text-2xl rounded-md flex justify-center items-center">
+                    +
+                </button>
+            </section>
         </div>
     )
 }
